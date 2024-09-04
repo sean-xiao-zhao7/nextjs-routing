94c53babb8127528b6554304344773c571eb17c6
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-09-04 17:07:41 -0400

    Add NewsPage content

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
 import { DUMMY_NEWS } from "../../data/news-articles";
+// import Image from "next/image";
 
 export default function NewsPage() {
     return (
         <>
             <h1>News Page</h1>
-            <ul>
+            <ul className="news-list">
                 {DUMMY_NEWS.map((newsArticle) => {
                     return (
                         <li key={newsArticle.id}>
-                            <Link href={`/news/${newsArticle.id}`}>
+                            <Link href={`/news/${newsArticle.slug}`}>
+                                <img
+                                    src={`/images/news/${newsArticle.image}`}
+                                    alt={newsArticle.title}
+                                />
+                            </Link>
+                            <Link href={`/news/${newsArticle.slug}`}>
                                 {newsArticle.title}
                             </Link>
                         </li>
