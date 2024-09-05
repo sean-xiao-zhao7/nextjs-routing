3bbb16f151ffb47167f41fad2c6555d78a7479e1
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-09-05 15:04:21 -0400

    Use getAvailableNewsYears in ArchivePage

--- app/archive/@archive/page.tsx
+++ app/archive/@archive/page.tsx
@@ -1,8 +1,19 @@
+import Link from "next/link";
+import { getAvailableNewsYears } from "../../../lib/news";
+
 export default function ArchivePage() {
+    const years = getAvailableNewsYears();
+
     return (
         <header id="archive-header">
             <nav>
-                <ul></ul>
+                <ul>
+                    {years.map((year) => (
+                        <li key={year}>
+                            <Link href={`/archive/${year}`}>{year}</Link>
+                        </li>
+                    ))}
+                </ul>
             </nav>
         </header>
     );
