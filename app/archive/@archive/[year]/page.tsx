a57f7847b15212662c3b61f48d1a0f9d3453c002
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-09-05 15:15:00 -0400

    Add filtering by year

--- app/archive/@archive/[year]/page.tsx
+++ app/archive/@archive/[year]/page.tsx
@@ -1,11 +1,18 @@
 import { DUMMY_NEWS } from "../../../../data/news-articles";
 import NewsList from "../../../components/NewList";
 
-export default function YearNewsPage({ year }: { year: number }) {
-    if (!year) {
+export default function YearNewsPage({ params }) {
+    if (!params.year) {
         throw Error("No year provided.");
     }
-    return (
-        <NewsList newsList={DUMMY_NEWS.filter((news) => news.year === year)} />
+
+    const newsList = DUMMY_NEWS.filter((news) =>
+        news.date.includes(params.year.toString())
     );
+
+    if (newsList.length <= 0) {
+        return <h1>No articles for year {params.year}.</h1>;
+    }
+
+    return <NewsList newsList={newsList} />;
 }
