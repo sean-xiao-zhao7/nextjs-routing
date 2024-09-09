98b9eb5edfc1600d02b229af0fe2df892fc7cf2a
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-09-09 16:47:32 -0400

    Add modal overlay

--- app/news/[slug]/layout.tsx
+++ app/news/[slug]/layout.tsx
@@ -1,3 +1,8 @@
-export default function NewsLayout({ children }) {
-    return <>{children}</>;
+export default function NewsLayout({ children, modal }) {
+    return (
+        <>
+            {modal}
+            {children}
+        </>
+    );
 }
