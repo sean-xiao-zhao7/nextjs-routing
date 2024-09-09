f46466a269fb901c39037eb93dc68233700ef035
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-09-09 16:13:01 -0400

    Add NewsLayout

--- app/news/[slug]/(.)image/page.tsx
+++ app/news/[slug]/(.)image/page.tsx
@@ -1,10 +1,24 @@
+import { notFound } from "next/navigation";
 import { DUMMY_NEWS } from "../../../../data/news-articles";
 
 export default function ImagePage({ params }) {
     const article = DUMMY_NEWS.find((article) => article.slug === params.slug);
+
+    if (!article) {
+        notFound();
+    }
+
     return (
-        <div className="fullscreen-image">
-            <img src={`/images/news/${article.image}`} alt={article.image} />
-        </div>
+        <>
+            <div className="model-backdrop" />
+            <dialog className="modal" open>
+                <div className="fullscreen-image">
+                    <img
+                        src={`/images/news/${article.image}`}
+                        alt={article.image}
+                    />
+                </div>
+            </dialog>
+        </>
     );
 }
--- app/news/[slug]/image/page.tsx
+++ app/news/[slug]/image/page.tsx
@@ -1,7 +1,13 @@
+import { notFound } from "next/navigation";
 import { DUMMY_NEWS } from "../../../../data/news-articles";
 
 export default function ImagePage({ params }) {
     const article = DUMMY_NEWS.find((article) => article.slug === params.slug);
+
+    if (!article) {
+        notFound();
+    }
+
     return (
         <div className="fullscreen-image">
             <img src={`/images/news/${article.image}`} alt={article.image} />
