e64d5a69433034c92cca09ead0e7c09badbe1689
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-09-11 15:13:18 -0400

    Adjust styling for button radii

--- app/(content)/error.tsx
+++ app/(content)/error.tsx
@@ -4,6 +4,7 @@ export default function ErrorPage() {
     return (
         <>
             <h1>Error.</h1>
+            <p>Error displaying the page.</p>
         </>
     );
 }
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -17,14 +17,7 @@ export default function HomePage() {
                 policies and international agreements aimed at reducing
                 greenhouse gas emissions. One of the most notable developments
                 in this arena is the continued decline in the cost of renewable
-                energy technologies.{" "}
-            </p>
-            <p>
-                According to the International Renewable Energy Agency (IRENA),
-                the cost of solar photovoltaic (PV) systems has dropped by over
-                80% in the past decade. This dramatic decrease is attributed to
-                technological advancements, economies of scale, and increased
-                competition within the market.
+                energy technologies.
             </p>
             <Link href="/news">Go to news index</Link>
         </div>
