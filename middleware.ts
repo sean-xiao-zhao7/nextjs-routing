992d406296c6753e3a797f03ce5ea371544c74ab
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-09-11 15:30:55 -0400

    Add middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export function middleware(request: Request) {
-    return NextResponse.next(request);
+export function middleware(request: NextRequest) {
+    // console.log(request);
+    return NextResponse.next();
 }
