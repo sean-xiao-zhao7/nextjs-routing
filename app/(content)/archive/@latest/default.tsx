8826e5ed609de7713ab7bbaa0316ed92d11033ae
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-09-13 14:01:52 -0400

    Fix archive page loaders

--- app/(content)/archive/@latest/default.tsx
+++ app/(content)/archive/@latest/default.tsx
@@ -1,11 +1,11 @@
 import { getLatestNews } from "@/lib/news";
 import NewsList from "@/app/components/NewList";
 
-export default function LatestPage() {
+export default async function LatestPage() {
     return (
         <>
             <h2>Latest</h2>
-            <NewsList newsList={getLatestNews()} />
+            <NewsList newsList={await getLatestNews()} />
         </>
     );
 }
