a3b015209af67a548aa4fb664e4c6c8ffcffd938
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-09-16 20:17:14 -0400

    Add styling for form upload

--- app/(testing)/testing/mutation/page.tsx
+++ app/(testing)/testing/mutation/page.tsx
@@ -1,7 +1,11 @@
-import { Button, Paper, TextField } from "@mui/material";
+import { Box, Button, Paper, TextField } from "@mui/material";
 import Stack from "@mui/material/Stack";
+
 import AddIcon from "@mui/icons-material/Add";
 import RestartAltIcon from "@mui/icons-material/RestartAlt";
+import InputLabel from "@mui/material/InputLabel";
+
+import UploadButton from "@/app/components/utils/VisuallyHiddenInput";
 
 export default function MutationTestIndexPage() {
     return (
@@ -10,16 +14,33 @@ export default function MutationTestIndexPage() {
             <Paper sx={{ padding: 2 }}>
                 <Stack spacing={2}>
                     <h2>Add a new post</h2>
-                    <TextField id="title" label="Title" variant="outlined" />
                     <TextField
-                        id="image-url"
-                        label="Image URL"
+                        id="title"
+                        label="Title"
                         variant="outlined"
+                        slotProps={{
+                            inputLabel: {
+                                shrink: true,
+                            },
+                        }}
                     />
+                    <Stack spacing={0} direction={"row"} marginTop={0}>
+                        <Box>
+                            <InputLabel htmlFor="input-with-icon-adornment">
+                                Image URL
+                            </InputLabel>
+                            <UploadButton id="title" text="Choose file" />
+                        </Box>
+                    </Stack>
                     <TextField
                         id="content"
                         label="Content"
                         variant="outlined"
+                        slotProps={{
+                            inputLabel: {
+                                shrink: true,
+                            },
+                        }}
                     />
                     <Stack spacing={1} direction={"row-reverse"}>
                         <Button variant="contained" startIcon={<AddIcon />}>
---
+++ app/components/utils/VisuallyHiddenInput.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+
+const VisuallyHiddenInput = styled("input")({
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
+    height: 1,
+    overflow: "hidden",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    whiteSpace: "nowrap",
+    width: 1,
+});
+
+export default function UploadButton(props) {
+    return (
+        <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<FileUploadIcon />}
+        >
+            {props.text}
+            <VisuallyHiddenInput
+                type="file"
+                onChange={(event) => console.log(event.target.files)}
+                multiple
+                id={props.id}
+            />
+        </Button>
+    );
+}
