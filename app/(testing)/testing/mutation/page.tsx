f7ad9cedac2c182d4dde30b06ae57ea6375e0a5c
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-09-19 15:10:01 -0400

    Add form around mutation testing inputs

--- app/(testing)/testing/mutation/page.tsx
+++ app/(testing)/testing/mutation/page.tsx
@@ -10,46 +10,52 @@ export default function MutationTestIndexPage() {
     return (
         <>
             <h1>Mutation testing page</h1>
-            <Paper sx={{ padding: 2 }}>
-                <Stack spacing={2}>
-                    <h2>Add a new post</h2>
-                    <TextField
-                        id="title"
-                        label="Title"
-                        variant="outlined"
-                        slotProps={{
-                            inputLabel: {
-                                shrink: true,
-                            },
-                        }}
-                    />
-                    <TextField
-                        id="content"
-                        label="Content"
-                        variant="outlined"
-                        slotProps={{
-                            inputLabel: {
-                                shrink: true,
-                            },
-                        }}
-                        multiline
-                        rows={5}
-                    />
-                    <Stack spacing={0} direction={"row"} marginTop={0}>
-                        <Box>
-                            <UploadButton id="title" text="Upload image" />
-                        </Box>
+            <form>
+                <Paper sx={{ padding: 2 }}>
+                    <Stack spacing={2}>
+                        <h2>Add a new post</h2>
+
+                        <TextField
+                            id="title"
+                            label="Title"
+                            variant="outlined"
+                            slotProps={{
+                                inputLabel: {
+                                    shrink: true,
+                                },
+                            }}
+                        />
+                        <TextField
+                            id="content"
+                            label="Content"
+                            variant="outlined"
+                            slotProps={{
+                                inputLabel: {
+                                    shrink: true,
+                                },
+                            }}
+                            multiline
+                            rows={5}
+                        />
+                        <Stack spacing={0} direction={"row"} marginTop={0}>
+                            <Box>
+                                <UploadButton id="title" text="Upload image" />
+                            </Box>
+                        </Stack>
+                        <Stack spacing={1} direction={"row-reverse"}>
+                            <Button variant="contained" startIcon={<AddIcon />}>
+                                Submit
+                            </Button>
+                            <Button
+                                variant="text"
+                                startIcon={<RestartAltIcon />}
+                            >
+                                Reset
+                            </Button>
+                        </Stack>
                     </Stack>
-                    <Stack spacing={1} direction={"row-reverse"}>
-                        <Button variant="contained" startIcon={<AddIcon />}>
-                            Submit
-                        </Button>
-                        <Button variant="text" startIcon={<RestartAltIcon />}>
-                            Reset
-                        </Button>
-                    </Stack>
-                </Stack>
-            </Paper>
+                </Paper>
+            </form>
         </>
     );
 }
