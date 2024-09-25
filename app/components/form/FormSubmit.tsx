ab59cdaaecffff6cb2ec59e44401d1b8330b0394
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-09-25 14:04:07 -0400

    Add loading to submit on mutation testing

--- app/components/form/FormSubmit.tsx
+++ app/components/form/FormSubmit.tsx
@@ -3,18 +3,25 @@
 import { useFormStatus } from "react-dom";
 import AddIcon from "@mui/icons-material/Add";
 import RestartAltIcon from "@mui/icons-material/RestartAlt";
-import { Button } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 export default function FormSubmit() {
     const status = useFormStatus();
+
     return (
         <>
-            <Button variant="contained" startIcon={<AddIcon />} type="submit">
+            <LoadingButton
+                variant="contained"
+                startIcon={<AddIcon />}
+                type="submit"
+                loading={status.pending}
+                loadingPosition="start"
+            >
                 Submit
-            </Button>
-            <Button variant="text" startIcon={<RestartAltIcon />}>
+            </LoadingButton>
+            <LoadingButton variant="text" startIcon={<RestartAltIcon />}>
                 Reset
-            </Button>
+            </LoadingButton>
         </>
     );
 }
