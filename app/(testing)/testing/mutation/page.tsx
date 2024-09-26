fe214ab08f77b4c9c23027368f3930016dfd920c
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-09-26 17:06:56 -0400

    Add error checking for post form

--- app/(testing)/testing/mutation/page.tsx
+++ app/(testing)/testing/mutation/page.tsx
@@ -6,20 +6,39 @@ import PostForm from "@/app/components/form/PostForm";
 export default async function MutationTestIndexPage() {
     async function mutationAction(_, formData: FormData) {
         "use server";
+        let errors = [];
+
         const title = formData.get("title");
         const content = formData.get("content");
         let imageFile = formData.get("image-file");
+
         let imageUrl = "";
         if (imageFile instanceof File && imageFile.size > 0) {
             imageUrl = imageFile.name;
         }
 
-        const userId = 1;
+        // error checking
+        if (!title || (title instanceof String && title.trim().length <= 0)) {
+            errors.push("Title is empty.");
+        }
+        if (
+            !content ||
+            (content instanceof String && content.trim().length <= 0)
+        ) {
+            errors.push("Content is empty.");
+        }
+        if (errors.length > 0) {
+            return { errors };
+        }
 
+        const userId = 1;
         const result = await storePost({ title, content, imageUrl, userId });
         if (!result) {
-            throw new Error("Error storing post with title " + title);
+            errors.push("Could not save post due to server error.");
+            return { errors };
         }
+
+        return { title, content, imageUrl, userId };
     }
 
     return <PostForm mutationAction={mutationAction} />;
