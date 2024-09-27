2fffbc0595232e840badc60720b3b2d2d5aac717
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-09-27 14:18:55 -0400

    Space out item

--- app/components/posts/SinglePostListItem.tsx
+++ app/components/posts/SinglePostListItem.tsx
@@ -1,12 +1,25 @@
-import { Paper } from "@mui/material";
+import { Paper, Stack } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 import { PostType } from "@/src/types/post-type";
 
 export default function SinglePostListItem({ post }: { post: PostType }) {
     return (
         <Paper sx={{ padding: 2 }}>
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
+            <Stack
+                direction="row"
+                spacing={2}
+                sx={{ justifyContent: "space-between" }}
+            >
+                <div>
+                    <h2>{post.title}</h2>
+                    <p>{post.content}</p>
+                </div>
+                <IconButton aria-label="delete">
+                    <DeleteIcon />
+                </IconButton>
+            </Stack>
         </Paper>
     );
 }
