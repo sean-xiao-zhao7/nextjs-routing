3b8a0fb27598ccf946485beb8bd0d76a4fee3d1a
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-10-07 15:04:56 -0400

    Migrate theme to root layout from testing layout
    This allows Roboto to apply to root children.

--- app/(testing)/layout.tsx
+++ app/(testing)/layout.tsx
@@ -1,22 +1,3 @@
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
-import { Roboto } from "next/font/google";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "@/src/theme";
-import "@/app/globals.css";
-
-const roboto = Roboto({
-    weight: ["300", "400", "500", "700"],
-    subsets: ["latin"],
-    display: "swap",
-    variable: "--font-roboto",
-});
-
 export default function RootLayout(props: React.PropsWithChildren) {
-    return (
-        <div className={roboto.variable}>
-            <AppRouterCacheProvider>
-                <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
-            </AppRouterCacheProvider>
-        </div>
-    );
+    return <div>{props.children}</div>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,29 @@
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import { Roboto } from "next/font/google";
+
+import theme from "@/src/theme";
+import "@/app/globals.css";
+
+const roboto = Roboto({
+    weight: ["300", "400", "500", "700"],
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--font-roboto",
+});
+
 export default function RootLayout(props: React.PropsWithChildren) {
     return (
         <html lang="en">
-            <body>{props.children}</body>
+            <body>
+                <main className={roboto.variable}>
+                    <AppRouterCacheProvider>
+                        <ThemeProvider theme={theme}>
+                            {props.children}
+                        </ThemeProvider>
+                    </AppRouterCacheProvider>
+                </main>
+            </body>
         </html>
     );
 }
