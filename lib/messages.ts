9ca8fc22f4921d669fd04099614c0aaa7fd38180
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-10-07 16:09:06 -0400

    Fix fetchAllMessages port bug

--- lib/messages.ts
+++ lib/messages.ts
@@ -2,7 +2,7 @@ export const fetchAllMessages = async () => {
     let messages = [];
     try {
         const result = await fetch(
-            `http://${process.env.BACKEND_BASE_URL}/${process.env.BACKEND_PORT}/messages`
+            `http://${process.env.BACKEND_BASE_URL}:${process.env.BACKEND_PORT}/messages`
         );
         const res = await result.json();
         messages = res;
