6e68ac28e08a1cd6ac101c09a4abf76e3dfa68b9
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-10-07 16:08:22 -0400

    Add error handling in fetchAllMessages

--- lib/messages.ts
+++ lib/messages.ts
@@ -1,11 +1,13 @@
 export const fetchAllMessages = async () => {
     let messages = [];
-    const result = await fetch(
-        `http://${process.env.BACKEND_BASE_URL}/${process.env.BACKEND_PORT}/messages`
-    );
-    const res = await result.json();
-    if (res) {
+    try {
+        const result = await fetch(
+            `http://${process.env.BACKEND_BASE_URL}/${process.env.BACKEND_PORT}/messages`
+        );
+        const res = await result.json();
         messages = res;
+        return res;
+    } catch (e) {
+        throw new Error("Unable to fetch from backend.");
     }
-    return messages;
 };
