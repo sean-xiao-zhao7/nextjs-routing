6784e05e9cdd1d3227eb0e7916464f22c6c1108b
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-10-09 17:01:38 -0400

    Use image remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: "seanxiao.com",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
