ff19559971349a6848df1de304883d0ae5f8bc53
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-10-10 16:51:30 -0400

    Add user DB functions

--- app/(testing)/testing/database/page.tsx
+++ app/(testing)/testing/database/page.tsx
@@ -1,13 +1,15 @@
+import { initUsersDb } from "@/lib/users";
 import { initPostsDb } from "@/src/lib/posts";
 import { CoffeeMaker } from "@mui/icons-material";
 import { Button } from "@mui/material";
 
 export default function DatabasePage() {
-    initPostsDb();
+    //initPostsDb();
+    initUsersDb();
 
     return (
         <>
-            <Button variant="contained" startIcon={<CoffeeMaker />}>
+            <Button variant="contained" startIcon={<CoffeeMaker />} disabled>
                 Manage Database
             </Button>
         </>
--- src/lib/users.ts
+++ src/lib/users.ts
@@ -5,36 +5,33 @@ const db = new sql("src/data/posts.db");
 
 export function initUsersDb() {
     db.exec(`
+    DROP TABLE users;
+    `);
+    db.exec(`
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY, 
       first_name TEXT, 
       last_name TEXT,
-      email TEXT,
+      username TEXT,
       password TEXT
     )`);
     db.exec(`
     CREATE TABLE IF NOT EXISTS sessions (
       id INTEGER PRIMARY KEY, 
-      image_url TEXT NOT NULL,
-      title TEXT NOT NULL, 
-      content TEXT NOT NULL, 
-      created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-      user_id INTEGER, 
-      FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+      expires_at INTEGER NOT NULL,
+      user_id INTEGER NOT NULL, 
+      FOREIGN KEY (user_id) REFERENCES users(id)
     )`);
 }
 
-initUsersDb();
-
-export const getSingleUser = cache(async function getSingleUser(maxNumber) {
+export const getSingleUserByUsernameDB = cache(async function getSingleUser(
+    username: string
+) {
     const stmt = db.prepare(`
-    SELECT posts.id, image_url AS image, title, content, created_at AS createdAt, first_name AS userFirstName, last_name AS userLastName, COUNT(likes.post_id) AS likes, EXISTS(SELECT * FROM likes WHERE likes.post_id = posts.id and likes.user_id = 2) AS isLiked
-    FROM posts
-    INNER JOIN users ON posts.user_id = users.id
-    LEFT JOIN likes ON posts.id = likes.post_id
-    GROUP BY posts.id
-    ORDER BY createdAt DESC
+    SELECT first_name, last_name, username
+    FROM users
+    WHERE username = ?
     `);
 
-    return stmt.all();
+    return stmt.get(username);
 });
