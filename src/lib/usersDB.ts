267d2727374bc304d6e7addd65c6801b7d9a7e9f
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-10-16 13:22:53 -0400

    Add hash functions

--- src/lib/usersDB.ts
+++ src/lib/usersDB.ts
@@ -1,5 +1,6 @@
 import { cache } from "react";
 import sql from "better-sqlite3";
+import crypto from "node:crypto";
 
 const db = new sql("src/data/posts.db");
 
@@ -35,3 +36,17 @@ export const getSingleUserByUsernameDB = cache(async function getSingleUser(
 
     return stmt.get(username);
 });
+
+export function hashUserPassword(password) {
+    const salt = crypto.randomBytes(16).toString("hex");
+
+    const hashedPassword = crypto.scryptSync(password, salt, 64);
+    return hashedPassword.toString("hex") + ":" + salt;
+}
+
+export function verifyPassword(storedPassword, suppliedPassword) {
+    const [hashedPassword, salt] = storedPassword.split(":");
+    const hashedPasswordBuf = Buffer.from(hashedPassword, "hex");
+    const suppliedPasswordBuf = crypto.scryptSync(suppliedPassword, salt, 64);
+    return crypto.timingSafeEqual(hashedPasswordBuf, suppliedPasswordBuf);
+}
