6fb6396c60fbbd60d48ce29e6e6cb54492a60a08
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-10-17 16:55:15 -0400

    Add password hide to register

--- app/components/form/RegisterForm.tsx
+++ app/components/form/RegisterForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 // react
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useFormState } from "react-dom";
 
 // mui
@@ -9,6 +9,13 @@ import Stack from "@mui/material/Stack";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Alert from "@mui/material/Alert";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
 
 // custom
 import FormSubmit from "@/app/components/form/FormSubmit";
@@ -16,6 +23,30 @@ import MyPaper from "../containers/MyPaper";
 import MyTextField from "./MyTextField";
 
 export default function RegisterForm({ mutationAction }) {
+    // control form fields because hide password logic requires it
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [terms, setTerms] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+
+    // password hide
+    const handleClickShowPassword = () => {
+        setShowPassword((show) => !show);
+    };
+    const handleMouseDownPassword = (
+        event: React.MouseEvent<HTMLButtonElement>
+    ) => {
+        event.preventDefault();
+    };
+    const handleMouseUpPassword = (
+        event: React.MouseEvent<HTMLButtonElement>
+    ) => {
+        event.preventDefault();
+    };
+
+    // reset form
     const formRef: React.RefObject<HTMLFormElement> = useRef();
     const resetFormHandler = () => {
         if (formRef && formRef.current) {
@@ -23,6 +54,7 @@ export default function RegisterForm({ mutationAction }) {
         }
     };
 
+    // useFormstate
     const [formState, formAction] = useFormState(mutationAction, {});
 
     return (
@@ -33,58 +65,92 @@ export default function RegisterForm({ mutationAction }) {
                         <MyTextField
                             id="username"
                             label="Username"
-                            variant="outlined"
-                            slotProps={{
-                                inputLabel: {
-                                    shrink: true,
-                                },
-                            }}
                             name="username"
                             required
-                        />
-                        <MyTextField
-                            id="password"
-                            label="Password"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
                             variant="outlined"
                             slotProps={{
                                 inputLabel: {
                                     shrink: true,
                                 },
                             }}
-                            name="password"
-                            required
-                            type="password"
                         />
+                        {/* endAdornment requires OutlinedInput instead of TextField */}
+                        <FormControl variant="outlined">
+                            <InputLabel htmlFor="password" shrink>
+                                Password *
+                            </InputLabel>
+                            <OutlinedInput
+                                id="password"
+                                name="password"
+                                label="Password"
+                                required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={
+                                                handleMouseDownPassword
+                                            }
+                                            onMouseUp={handleMouseUpPassword}
+                                            edge="end"
+                                        >
+                                            {showPassword ? (
+                                                <VisibilityOff />
+                                            ) : (
+                                                <Visibility />
+                                            )}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                notched
+                                type={showPassword ? "text" : "password"}
+                                sx={{ borderRadius: 0 }}
+                            />
+                        </FormControl>
                         <MyTextField
                             id="firstname"
-                            label="Firstname"
+                            label="First Name"
+                            name="firstname"
+                            value={firstname}
+                            onChange={(e) => setFirstname(e.target.value)}
+                            required
                             variant="outlined"
                             slotProps={{
                                 inputLabel: {
                                     shrink: true,
                                 },
                             }}
-                            name="firstname"
-                            required
                         />
                         <MyTextField
                             id="lastname"
-                            label="Lastname"
+                            label="Last Name"
+                            name="lastname"
+                            required
+                            value={lastname}
+                            onChange={(e) => setLastname(e.target.value)}
                             variant="outlined"
                             slotProps={{
                                 inputLabel: {
                                     shrink: true,
                                 },
                             }}
-                            name="lastname"
-                            required
                         />
                         <FormControlLabel
                             required
-                            control={<Checkbox />}
+                            control={
+                                <Checkbox
+                                    id="terms"
+                                    name="terms"
+                                    value={terms}
+                                    onChange={() => setTerms((term) => !term)}
+                                />
+                            }
                             label="Accept terms of service to register."
-                            id="terms"
-                            name="terms"
                         />
                         {formState.errorMessage && (
                             <Alert severity="error">
