9ea3ef9934a7e9cf79584bad6757bde96198f66e
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-10-18 14:21:15 -0400

    Add primary palette color

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,20 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
+    palette: {
+        primary: {
+            light: "#238939",
+            main: "#18782f",
+            dark: "#01591c",
+            contrastText: "#fff",
+        },
+        secondary: {
+            light: "#ff7961",
+            main: "#f44336",
+            dark: "#ba000d",
+            contrastText: "#000",
+        },
+    },
     typography: {
         fontFamily: "var(--font-roboto)",
     },
