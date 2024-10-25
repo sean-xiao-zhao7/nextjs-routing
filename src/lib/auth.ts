c12ef0b2266b405a4b1db527f5c66c909d6fecb7
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-10-25 13:40:13 -0400

    Add cookie functions to auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,8 +4,29 @@ import {
 } from "@oslojs/encoding";
 import sql from "better-sqlite3";
 import { sha256 } from "@oslojs/crypto/sha2";
+import { cookies } from "next/headers";
 
 const db = new sql("src/data/posts.db");
+
+export function setSessionTokenCookie(token: string, expiresAt: Date): void {
+    cookies().set("session", token, {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+        expires: expiresAt,
+        path: "/",
+    });
+}
+
+export function deleteSessionTokenCookie(): void {
+    cookies().set("session", "", {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 0,
+        path: "/",
+    });
+}
 
 export function generateSessionToken(): string {
     const bytes = new Uint8Array(20);
