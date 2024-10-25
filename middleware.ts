31fc613aa9a5935869319f022510904491dc9e51
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-10-25 13:37:01 -0400

    Add csrf middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,30 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    // console.log(request);
+    if (request.method === "GET") {
+        return NextResponse.next();
+    }
+    const originHeader = request.headers.get("Origin");
+    // NOTE: You may need to use `X-Forwarded-Host` instead
+    const hostHeader = request.headers.get("Host");
+    if (originHeader === null || hostHeader === null) {
+        return new NextResponse(null, {
+            status: 403,
+        });
+    }
+    let origin: URL;
+    try {
+        origin = new URL(originHeader);
+    } catch {
+        return new NextResponse(null, {
+            status: 403,
+        });
+    }
+    if (origin.host !== hostHeader) {
+        return new NextResponse(null, {
+            status: 403,
+        });
+    }
     return NextResponse.next();
 }
 
