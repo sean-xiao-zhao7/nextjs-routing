6db684996235f6e46fca17a7ba952aa367876156
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-11-01 15:33:35 -0400

    Protect testing pages from unauth

--- app/(testing)/testing/layout.tsx
+++ app/(testing)/testing/layout.tsx
@@ -1,8 +1,15 @@
 import { fetchAllMessages } from "@/src/lib/messages";
 
 import MainHeader from "@/app/components/headers/MainHeader";
+import { getCurrentSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function TestingLayout(props: React.PropsWithChildren) {
+    const currentSession = await getCurrentSession();
+    if (!currentSession.session) {
+        redirect("/login");
+    }
+
     let messagesElement = null;
     try {
         const messages = await fetchAllMessages("testing layout");
