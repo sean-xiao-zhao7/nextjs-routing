23d22e1741a03cf247e3ec3cc122b0ebb9d59219
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-11-05 16:07:32 -0500

    Add global-error

---
+++ app/global-error.tsx
@@ -0,0 +1,16 @@
+export default function GlobalError({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
+    return (
+        <html>
+            <body>
+                <h1>Error</h1>
+                <p>Please try again later.</p>
+            </body>
+        </html>
+    );
+}
