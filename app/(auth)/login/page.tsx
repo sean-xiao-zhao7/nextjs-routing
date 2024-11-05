9e39ec3a1076586b0f768922cda99a6b41ba90a0
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-11-05 16:32:06 -0500

    Use streaming for login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,16 @@
+import { Suspense } from "react";
+import { LinearProgress } from "@mui/material";
+
 import LoginForm from "@/app/components/form/LoginForm";
 import { loginAction } from "@/lib/usersServerActions";
 
 export default function LoginPage() {
     return (
         <>
-            <h1>Login</h1>
-            <LoginForm mutationAction={loginAction} />
+            <Suspense fallback={<LinearProgress />}>
+                <h1>Login</h1>
+                <LoginForm mutationAction={loginAction} />
+            </Suspense>
         </>
     );
 }
