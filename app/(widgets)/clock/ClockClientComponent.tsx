2f940fa9a9c33a1f6732e7ea997018fb4fc0a623
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-03-14 14:55:55 -0400

    Add ClockClientComponent time retrieval

--- app/(widgets)/clock/ClockClientComponent.tsx
+++ app/(widgets)/clock/ClockClientComponent.tsx
@@ -1,9 +1,32 @@
 "use client";
 
+import { useState, useEffect } from "react";
+
 export default function ClockClientComponent() {
+    const [time, setTime] = useState([0, 0, 0, 0, 0, 0]);
+
+    const getCurrentTime = () => {
+        const dateObj = new Date();
+        const hours = dateObj.getHours();
+        const mins = dateObj.getMinutes();
+        const secs = dateObj.getSeconds();
+        setTime([
+            Math.floor(hours / 10),
+            hours % 10,
+            Math.floor(mins / 10),
+            mins % 10,
+            Math.floor(secs / 10),
+            secs % 10,
+        ]);
+    };
+
+    useEffect(() => {
+        setInterval(getCurrentTime, 1000);
+    }, []);
+
     return (
         <>
-            <div id="clock">Clock</div>
+            <div id="clock">{time}</div>
         </>
     );
 }
