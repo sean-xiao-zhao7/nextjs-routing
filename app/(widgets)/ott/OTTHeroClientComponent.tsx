ca14de063a242732e4cba192da819804f4669dc3
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-04-07 16:29:46 -0400

    Complete carousel

--- app/(widgets)/ott/OTTHeroClientComponent.tsx
+++ app/(widgets)/ott/OTTHeroClientComponent.tsx
@@ -11,6 +11,28 @@ import heroImage3 from "@/assets/img/ott/hero-image3.png";
 import heroImage4 from "@/assets/img/ott/hero-image4.png";
 
 export default function OTTHeroClientComponent() {
+    const [currentImage, setCurrentImage] = useState(0);
+    const images = [heroImage1, heroImage2, heroImage3, heroImage4];
+    let intervalVar;
+
+    useEffect(() => {
+        intervalVar = setInterval(() => {
+            setCurrentImage((currentImage) => {
+                if (currentImage === images.length - 1) {
+                    return 0;
+                } else {
+                    return currentImage + 1;
+                }
+            });
+        }, 3000);
+
+        return () => {
+            clearInterval(intervalVar);
+        };
+    }, []);
+
+    console.log(currentImage);
+
     return (
         <div id="ott-hero">
             <div id="ott-hero-inner">
@@ -18,7 +40,12 @@ export default function OTTHeroClientComponent() {
                 <p>Put your trust in us. A bright future awaits.</p>
                 <a className="ott-button white-button">Learn more</a>
             </div>
-            <Image src={heroImage1} alt="hero image 1" fill />
+            <Image
+                src={images[currentImage]}
+                alt="hero image 1"
+                fill
+                placeholder="blur"
+            />
         </div>
     );
 }
