3a25fcbe83976c72723554969743fa42447b1f2d
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-04-08 14:30:13 -0400

    Reinstate login/register after adding DBs.

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,14 +1,14 @@
 import { Suspense } from "react";
 import { LinearProgress } from "@mui/material";
 
-// import RegisterForm from "@/app/components/form/RegisterForm";
-// import { registerAction } from "@/lib/usersServerActions";
+import RegisterForm from "@/app/components/form/RegisterForm";
+import { registerAction } from "@/lib/usersServerActions";
 
 export default function RegisterPage() {
     return (
         <Suspense fallback={<LinearProgress />}>
             <h1>Register</h1>
-            {/* <RegisterForm mutationAction={registerAction} /> */}
+            <RegisterForm mutationAction={registerAction} />
         </Suspense>
     );
 }
